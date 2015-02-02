0b2616c8f017012cec6fc1ed18a9ee7dc8ca1829
Ian Gawronski <iangawronski@gmail.com>
2015-02-02 17:47:41 -0500

    still working through HW. First push

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20150202162438) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
